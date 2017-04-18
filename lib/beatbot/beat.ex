24f90e8f37f06a6dacdc8f9c397049211188a15b
David Phillips <david@daviddouglasphillips.com>
2017-04-18 16:35:48 -0700

    Hack around rolling over beat time

--- lib/beatbot/beat.ex
+++ lib/beatbot/beat.ex
@@ -1,8 +1,14 @@
 defmodule Beatbot.Beat do
   def beat() do
-    DateTime.utc_now()
-    |> (&((&1.second + (&1.minute * 60) + (((&1.hour + 1) * 3600))) / 86.4)).()
-    |> Float.round(1)
-    |> Float.to_string
+    beats =
+      DateTime.utc_now()
+      |> (&((&1.second + (&1.minute * 60) + (((&1.hour + 1) * 3600))) / 86.4)).()
+      |> Float.round(3)
+
+    if beats >= 1000.000 do
+      "0" <> (Float.round((beats - 1000.000), 3) |> Float.to_string())
+    else
+      beats |> Float.to_string()
+    end
   end
 end
